7a61b2a206d5e50a44a6f15bc60f2f91cd7b1c28
Ryu-Sen <sendaryunosuke.gmail.com>
2025-11-08 21:02:49 +0900

    defeat logic

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -9,3 +9,7 @@ class_name Enemy
 func _ready() -> void:
     rig.set_active_mesh(rig.villager_meshes.pick_random())
     health_component.update_max_health(max_health)
+
+func _on_health_component_defeat() -> void:
+    rig.travel("Defeat")
+    # queue_free()
